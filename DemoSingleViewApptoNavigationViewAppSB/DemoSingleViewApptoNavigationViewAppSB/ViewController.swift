5dba385f661b747516b54cd805d4a966ad58ef9a
徐涛 <xutao@TennyXu.local>
2017-07-07 11:28:17 +0800

    Friday, 7 July 2017 at 11:28:08

---
+++ DemoSingleViewApptoNavigationViewAppSB/DemoSingleViewApptoNavigationViewAppSB/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DemoSingleViewApptoNavigationViewAppSB
+//
+//  Created by 徐涛 on 07/07/2017.
+//  Copyright © 2017 徐涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "DemoSingleViewApptoNavigationViewAppSB"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DemoSingleViewApptoNavigationViewAppSB
 将iOS  Single View App 转换为iOS Navigation View App（StroryBoard方式）
+
+方式1：StroryBoard方式，步骤如下：
+
+1. 在SB中，选中ViewController Sence，插入Navigation Controller（Editor-->Embed in-->Navigation Controller）
+
+2. 添加标题
+self.title = "DemoSingleViewApptoNavigationViewAppSB"
+
+3. 运行结果显示，Single View App已经转换为了Navigation View App
